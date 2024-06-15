af612ce3e920448689d8db566adfa912efd10184
ighor.lima <ighor.lima@totvs.com.br>
2024-06-15 16:41:03 -0300

    AeroFusion

--- PROJETO 1/js/pagamento.js
+++ PROJETO 1/js/pagamento.js
@@ -4,6 +4,8 @@ var Tela = document.getElementsByTagName('main')[0];
 var carregamento = document.getElementById('tela_carregamento');
 var aviso = document.getElementsByClassName('alertas');
 var aviso_texto = document.getElementsByClassName('alerta_texto');
+//Objeto
+var ajax = null;
 
 //-------------------------------Eventos
 /*
@@ -13,20 +15,7 @@ var aviso_texto = document.getElementsByClassName('alerta_texto');
 * Data: 14/06/2024
 */
 window.addEventListener('DOMContentLoaded', ()=>{
-	telaCarregamento(true);//Carrega dados do pedido
-    $.ajax({
-        url: 'script/pedidos.php?Opc=2',
-        method: 'GET',
-        dataType: 'html',
-        success: function (data) {
-            $(Tela).html(data); // Insere os dados no elemento main
-            telaCarregamento(false); // Desliga a tela de carregamento
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            alerta('Houve um erro ao carregar o seu pedido. Tente novamente ou mais tarde.', 0);
-            telaCarregamento(false); // Desliga a tela de carregamento em caso de erro
-        }
-    });
+    novoPedido();
 })
 //-------------------------------Funções
 /*
@@ -70,4 +59,51 @@ Programador: Ighor Drummond
 function fecharAlerta(alerta) {
     aviso[alerta].classList.remove('d-block');
     aviso[alerta].classList.add('d-none');
+}
+/*
+Função: deletarItem()
+Descrição: Responsavel por deletar items do carrinho e do pedido
+Data: 15/06/2024
+Programador: Ighor Drummond
+*/
+function deletaItem(Item){
+    telaCarregamento(true);
+    //Apaga da session o produto
+    //Apaga do carrinho o produto
+    //Recarrega a página usando a função dom
+    ajax = new XMLHttpRequest();
+    ajax.open('POST', 'script/pedidos.php?Opc=3&Prod=' + encodeURIComponent(Item));
+    ajax.onreadystatechange = ()=>{
+        telaCarregamento(false);
+        if(ajax.readyState === 4){
+            if(ajax.status < 400){
+                novoPedido();//Chama para carregar os dados novamente sem o item
+            }else{
+                window.location.href = "error.php?Error=" + ajax.status.toString();
+            }
+        }
+    }
+    ajax.send();
+}
+/*
+Função: novoPedido()
+Descrição: Responsavel por carregar dados do pagamentos
+Data: 15/06/2024
+Programador: Ighor Drummond
+*/
+function novoPedido(){
+    telaCarregamento(true);//Carrega dados do pedido
+    $.ajax({
+        url: 'script/pedidos.php?Opc=2',
+        method: 'GET',
+        dataType: 'html',
+        success: function (data) {
+            $(Tela).html(data); // Insere os dados no elemento main
+            telaCarregamento(false); // Desliga a tela de carregamento
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alerta('Houve um erro ao carregar o seu pedido. Tente novamente ou mais tarde.', 0);
+            telaCarregamento(false); // Desliga a tela de carregamento em caso de erro
+        }
+    });
 }
