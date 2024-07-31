a9b7e11b1a1891ba322529875b292dc069beb283
ighor.lima <ighor.lima@totvs.com.br>
2024-07-31 17:08:32 -0300

    AeroFusion

---
+++ PROJETO 1/assuntos.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php require_once('script/style.php'); ?>
+		<!-- Estilo da Página -->
+		<link rel="stylesheet" type="text/css" href="css/assuntos.css">
+		<!-- Titulo da página -->
+		<title>AeroFusion - Assuntos</title>
+	</head>
+	<body>
+		<?php require_once('script/cabecalho.php'); ?>
+		<main class="p-1">
+			<section id="prazo" class="formatar_texto">
+				<h1>Prazo de Entrega da AeroFusion</h1>
+				<p>
+					A AeroFusion se dedica a fornecer produtos de alta qualidade e serviços excepcionais aos seus clientes. Nosso compromisso com a excelência se estende ao nosso sistema de entregas, garantindo que você receba suas encomendas de maneira rápida e segura.
+
+					Prazo de Processamento
+					Após a confirmação do pagamento, nosso time de logística se empenha para processar e embalar seu pedido o mais rapidamente possível. O prazo médio de processamento é de 1 a 2 dias úteis, durante os quais verificamos a qualidade dos produtos e preparamos a embalagem para envio.
+
+					Prazo de Entrega
+					O prazo de entrega pode variar dependendo da sua localização e do método de envio escolhido. Trabalhamos com transportadoras confiáveis para assegurar que sua encomenda chegue ao destino com rapidez e segurança. Os prazos estimados são:
+
+					Entregas Nacionais:
+
+					Regiões Metropolitanas: 3 a 5 dias úteis
+					Outras Regiões: 5 a 10 dias úteis
+					Entregas Internacionais:
+
+					América do Norte e Europa: 7 a 14 dias úteis
+					Outros Continentes: 10 a 21 dias úteis
+					Rastreamento de Pedidos
+					Para sua conveniência, todos os pedidos incluem um número de rastreamento que será enviado por e-mail assim que o pedido for despachado. Você pode acompanhar o status da entrega em tempo real através do site da transportadora.
+
+					Fatores que Podem Influenciar o Prazo de Entrega
+					Alguns fatores externos podem influenciar o prazo de entrega, como feriados locais, condições climáticas adversas e procedimentos alfandegários para encomendas internacionais. A AeroFusion se esforça para mitigar quaisquer atrasos, mantendo uma comunicação transparente com nossos clientes.
+
+					Suporte ao Cliente
+					Estamos comprometidos em proporcionar a melhor experiência possível. Caso tenha alguma dúvida ou encontre qualquer problema com a entrega do seu pedido, nossa equipe de suporte ao cliente está à disposição para ajudar. Entre em contato conosco através dos nossos canais de atendimento e teremos prazer em assisti-lo.
+
+					A AeroFusion agradece sua confiança e preferência. Trabalhamos continuamente para melhorar nossos serviços e garantir que suas encomendas cheguem sempre no prazo e em perfeito estado.					
+				</p>
+			</section>
+			<section id="troca" class="formatar_texto">
+				<h1>Política de Trocas e Devoluções da AeroFusion</h1>
+				<p>
+					Na AeroFusion, nossa prioridade é garantir a satisfação de nossos clientes. Para isso, estabelecemos uma política de trocas e devoluções que oferece segurança e tranquilidade na sua experiência de compra. Abaixo, detalhamos os procedimentos para efetuar trocas ou devoluções de produtos adquiridos em nossa loja.
+
+					Prazo para Trocas e Devoluções
+					Trocas: Caso você precise trocar um produto por outro modelo, cor ou tamanho, você tem até 30 dias corridos, a contar da data de recebimento do produto, para solicitar a troca.
+					Devoluções: Para devolução por arrependimento ou desistência, o prazo é de 7 dias corridos, também a partir da data de recebimento do produto.
+					Condições para Trocas e Devoluções
+					Para que a troca ou devolução seja aceita, é necessário que o produto esteja nas seguintes condições:
+
+					Em sua embalagem original, sem indícios de uso, sem violação do lacre original do fabricante.
+					Acompanhado da nota fiscal, manuais, certificados de garantia e todos os acessórios que acompanham o produto.
+					Sem quaisquer sinais de dano por mau uso ou desgaste excessivo.
+					Procedimento para Solicitação
+					Contato Inicial: Entre em contato com nossa equipe de atendimento ao cliente através do e-mail email@example.com ou pelo telefone (XX) XXXX-XXXX. Informe seu nome, número do pedido, produto a ser trocado ou devolvido e o motivo da solicitação.
+
+					Análise da Solicitação: Após receber sua solicitação, nossa equipe analisará o pedido e fornecerá as orientações necessárias para o envio do produto de volta à AeroFusion. Caso a devolução seja por defeito, pedimos que envie fotos do produto para agilizar a análise.
+
+					Envio do Produto: Após a aprovação da solicitação, encaminhe o produto para o endereço fornecido pela nossa equipe. O custo do envio inicial será reembolsado em casos de devolução por defeito ou erro na entrega.
+
+					Recebimento e Análise do Produto: Assim que o produto for recebido em nosso centro de distribuição, será feita uma análise para verificar se está em conformidade com as condições mencionadas. Esta análise pode levar até 5 dias úteis.
+
+					Conclusão:
+
+					Troca: Em caso de troca, após a confirmação de que o produto está em conformidade, o novo item será enviado para você, e os custos de envio serão de responsabilidade da AeroFusion.
+					Devolução: Em caso de devolução, após a confirmação de que o produto está em conformidade, o valor pago será reembolsado de acordo com a forma de pagamento utilizada na compra. Este processo pode levar até 10 dias úteis após a análise do produto.
+					Itens Não Elegíveis para Troca ou Devolução
+					Produtos personalizados ou feitos sob encomenda.
+					Produtos em liquidação ou com desconto, salvo em caso de defeito.
+					Itens que apresentem sinais de uso inadequado, desgaste excessivo ou que não atendam às condições mencionadas.
+					Atendimento ao Cliente
+					Estamos à disposição para esclarecer qualquer dúvida e fornecer o suporte necessário durante todo o processo de troca ou devolução. Entre em contato conosco através dos nossos canais de atendimento, e nossa equipe terá prazer em assisti-lo.
+
+					A AeroFusion agradece sua confiança e preferência. Nosso compromisso é garantir sua total satisfação, proporcionando uma experiência de compra segura, ágil e transparente.					
+				</p>	
+			</section>
+			<section id="perguntas" class="formatar_texto">
+				<h1>Perguntas Frequentes da AeroFusion</h1>
+				<p>
+					Bem-vindo à seção de Perguntas Frequentes da AeroFusion. Aqui, você encontrará respostas para as perguntas mais comuns sobre nossos produtos e serviços. Caso não encontre a resposta que procura, sinta-se à vontade para entrar em contato com nossa equipe de atendimento ao cliente.
+
+					1. Como posso rastrear meu pedido?
+					Após o envio do seu pedido, você receberá um e-mail com um número de rastreamento. Você pode usar esse número no site da transportadora para acompanhar o status da entrega em tempo real.
+
+					2. Qual é o prazo de entrega dos produtos?
+					O prazo de entrega pode variar dependendo da sua localização:
+
+					Regiões Metropolitanas: 3 a 5 dias úteis.
+					Outras Regiões Nacionais: 5 a 10 dias úteis.
+					Entregas Internacionais: 7 a 21 dias úteis, dependendo do continente.
+					3. Posso trocar um produto?
+					Sim, você pode solicitar a troca de um produto em até 30 dias corridos após o recebimento. O produto deve estar em sua embalagem original, sem indícios de uso e com todos os acessórios e manuais.
+
+					4. Como faço para devolver um produto?
+					Você pode devolver um produto em até 7 dias corridos após o recebimento, caso esteja insatisfeito ou tenha mudado de ideia. O produto deve estar nas mesmas condições em que foi recebido, sem sinais de uso, na embalagem original e com todos os acessórios e manuais.
+
+					5. O que fazer se o produto chegar com defeito?
+					Se o produto chegar com defeito, entre em contato com nossa equipe de atendimento ao cliente imediatamente. Envie fotos do produto e a nota fiscal para que possamos analisar o caso e providenciar a troca ou devolução sem custo adicional.
+
+					6. Quais formas de pagamento são aceitas?
+					Aceitamos diversas formas de pagamento, incluindo cartões de crédito, cartões de débito, boletos bancários e transferências eletrônicas. As opções disponíveis serão exibidas durante o processo de compra.
+
+					7. É seguro comprar na AeroFusion?
+					Sim, nossa loja utiliza tecnologia de criptografia para proteger seus dados pessoais e financeiros. Trabalhamos com parceiros confiáveis e estamos comprometidos em garantir uma experiência de compra segura para nossos clientes.
+
+					8. Como posso entrar em contato com o atendimento ao cliente?
+					Você pode entrar em contato com nossa equipe de atendimento ao cliente através do e-mail email@example.com ou pelo telefone (XX) XXXX-XXXX. Nosso horário de atendimento é de segunda a sexta, das 9h às 18h.
+
+					9. A AeroFusion oferece garantia nos produtos?
+					Sim, todos os nossos produtos possuem garantia contra defeitos de fabricação. O período de garantia varia de acordo com o produto e está especificado na descrição de cada item.
+
+					10. Como posso cancelar meu pedido?
+					Para cancelar um pedido, entre em contato com nossa equipe de atendimento ao cliente o mais rápido possível. Se o pedido ainda não tiver sido despachado, podemos processar o cancelamento e reembolso. Caso o pedido já tenha sido enviado, você poderá recusar o recebimento ou solicitar a devolução após recebê-lo.
+
+					Esperamos que estas respostas tenham sido úteis. A AeroFusion está sempre à disposição para esclarecer suas dúvidas e proporcionar a melhor experiência de compra possível. Agradecemos a sua confiança e preferência.					
+				</p>
+			</section>
+			<section id="termos" class="formatar_texto">
+				<h1>Termos de Uso e Privacidade da AeroFusion</h1>
+				<p>
+					Termos de Uso
+					Bem-vindo ao site da AeroFusion. Ao acessar e utilizar nosso site, você concorda com os termos e condições estabelecidos abaixo. Por favor, leia atentamente os termos de uso antes de utilizar nossos serviços.
+
+					1. Aceitação dos Termos
+					Ao utilizar o site da AeroFusion, você concorda em cumprir e estar vinculado aos termos e condições deste acordo. Se você não concordar com estes termos, por favor, não utilize o site.
+
+					2. Uso do Site
+					Você concorda em utilizar o site apenas para fins lícitos e de maneira que não infrinja os direitos de terceiros, nem restrinja ou iniba o uso e aproveitamento do site por qualquer outra pessoa. Atividades proibidas incluem assédio, difamação ou conduta que possa causar transtornos a outros usuários.
+
+					3. Propriedade Intelectual
+					Todo o conteúdo do site, incluindo textos, gráficos, logotipos, ícones, imagens, clipes de áudio, downloads digitais e software, é de propriedade da AeroFusion ou de seus fornecedores de conteúdo e protegido pelas leis de direitos autorais internacionais. A compilação de todo o conteúdo do site é de propriedade exclusiva da AeroFusion e protegida por leis de direitos autorais.
+
+					4. Links para Outros Sites
+					Nosso site pode conter links para sites de terceiros que não são operados ou controlados pela AeroFusion. Não somos responsáveis pelo conteúdo, políticas de privacidade ou práticas de qualquer site de terceiros. Recomendamos que você leia os termos e condições e as políticas de privacidade de qualquer site de terceiros que visitar.
+
+					5. Limitação de Responsabilidade
+					A AeroFusion não será responsável por quaisquer danos diretos, indiretos, incidentais, consequenciais ou punitivos resultantes do uso ou da incapacidade de usar este site ou os serviços nele contidos.
+
+					6. Modificações nos Termos de Uso
+					A AeroFusion reserva-se o direito de alterar estes termos de uso a qualquer momento. Quaisquer alterações serão publicadas nesta página. O uso continuado do site após a publicação de alterações constitui sua aceitação dos novos termos de uso.
+
+					Política de Privacidade
+					A AeroFusion valoriza sua privacidade e está comprometida em proteger suas informações pessoais. Esta política de privacidade descreve como coletamos, usamos, e protegemos suas informações.
+
+					1. Coleta de Informações
+					Coletamos informações pessoais que você nos fornece voluntariamente ao registrar uma conta, fazer uma compra, ou entrar em contato conosco. As informações podem incluir nome, endereço de e-mail, endereço de entrega, número de telefone e informações de pagamento.
+
+					2. Uso das Informações
+					Usamos as informações coletadas para:
+
+					Processar e gerenciar suas compras.
+					Melhorar nossos produtos e serviços.
+					Enviar atualizações sobre seu pedido.
+					Enviar ofertas promocionais e newsletters, se você tiver optado por recebê-las.
+					3. Compartilhamento de Informações
+					Não vendemos, trocamos ou alugamos suas informações pessoais para terceiros. Podemos compartilhar suas informações com fornecedores de serviços de confiança que auxiliam na operação do nosso site, conduzem nossos negócios ou atendem você, desde que esses terceiros concordem em manter essas informações confidenciais.
+
+					4. Proteção das Informações
+					Implementamos uma variedade de medidas de segurança para manter a segurança de suas informações pessoais. Suas informações pessoais são armazenadas em redes seguras e só são acessíveis por um número limitado de pessoas que têm direitos especiais de acesso a esses sistemas e são obrigadas a manter a confidencialidade das informações.
+
+					5. Cookies
+					Nosso site pode utilizar cookies para melhorar sua experiência. Os cookies são pequenos arquivos que um site ou seu provedor de serviços transfere para o disco rígido do seu computador através do navegador da web (se você permitir) que permite que os sistemas do site ou do provedor de serviços reconheçam seu navegador e capturem e lembrem certas informações.
+
+					6. Seus Direitos
+					Você tem o direito de acessar, corrigir ou excluir suas informações pessoais armazenadas por nós. Para fazer isso, entre em contato com nosso suporte ao cliente.
+
+					7. Alterações na Política de Privacidade
+					Podemos atualizar nossa política de privacidade periodicamente. Notificaremos você sobre quaisquer alterações publicando a nova política de privacidade em nosso site. Recomendamos que você revise esta política periodicamente para quaisquer alterações.
+
+					Se você tiver alguma dúvida sobre nossos termos de uso ou política de privacidade, por favor, entre em contato conosco pelo e-mail aerofusion@gmail.com ou pelo telefone (XX) XXXX-XXXX. Agradecemos por escolher a AeroFusion.					
+				</p>		
+			</section>
+		</main>
+		<?php require_once('script/rodape.php'); ?>
+		<?php require_once('script/scripts.php'); ?>
+	</body>
+</html>
