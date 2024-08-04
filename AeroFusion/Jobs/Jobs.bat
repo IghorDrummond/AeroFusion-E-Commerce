6ce4a8125de4a30fba6e4a2ee4c2d9c76f7a6a2e
ighor.lima <ighor.lima@totvs.com.br>
2024-08-04 17:08:37 -0300

    AeroFusion

---
+++ AeroFusion/Jobs/Jobs.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Caminho para o executável do PHP
+set PHP_PATH=C:\xampp\php\php.exe
+
+REM Caminho para o script PHP
+set SCRIPT_PATH=C:\Users\Drummond\Documents\FullStack\Gits\AeroFusion-E-Commerce\AeroFusion\Jobs\normalizaProdutos.php
+
+REM Executa o script PHP
+%PHP_PATH% %SCRIPT_PATH%
